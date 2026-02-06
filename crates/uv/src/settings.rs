00e529753c6c697774c50d813c02e6998c2d978f
Tomasz (Tom) Kramkowski <tom@astral.sh>
2026-02-06 12:47:28 +0000

    Explicit index warning

--- crates/uv/src/settings.rs
+++ crates/uv/src/settings.rs
@@ -1932,6 +1932,7 @@ impl AddSettings {
             filesystem.as_ref(),
             package_indexes.clone(),
             preview,
+            true,
         )
         .into_iter()
         .flatten()
